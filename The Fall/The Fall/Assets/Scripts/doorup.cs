926ec42c5cefc90974168d646d5a463fb77ad9ed
UAD\1705118 <1705118@uad.ac.uk>
2018-09-26 16:37:22 +0100

    new vesion

---
+++ The Fall/The Fall/Assets/Scripts/doorup.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorup : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject trangle;
+    private bool able;
+    public float speed = 5.0f;
+    private bool movable = false;
+    //public float speedz;
+    // Use this for initialization
+    void Start()
+    {
+        able = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (door.transform.position.y < 5)
+        {
+        if (movable == true)
+        {
+
+            door.transform.Translate(Vector3.up * Time.deltaTime * speed, Space.Self);
+            
+
+        }
+        }
+
+        if (trangle.transform.position.y > -1)
+        {
+            if (movable == true)
+            {
+                trangle.transform.Translate(Vector3.down * Time.deltaTime * speed, Space.Self);
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+
+    {
+        if (collision.collider.gameObject.tag == "ball" && able == true)
+        {
+            //speedz = speed * Time.deltaTime;
+            
+            able = false;
+            movable = true;
+
+        }
+
+
+    }
+}
---
+++ The Fall/The Fall/Assets/Scripts/rotacube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacube : MonoBehaviour {
+
+
+    public Transform cube;
+    public float speed = 4.0f;
+    private bool _mouseDown = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            
+            cube.Rotate(Vector3.back, Time.deltaTime * speed, Space.World);
+
+        
+    }
+
+
+	}
+

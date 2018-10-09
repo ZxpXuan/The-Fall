ba500f410460aaa6976c900bf0fec09e97148857
ZxpXuan <38170958+ZxpXuan@users.noreply.github.com>
2018-10-09 16:41:56 +0100

    Add more effect for bouncing

--- The Fall/The Fall/Assets/Scripts/limitation.cs
+++ The Fall/The Fall/Assets/Scripts/limitation.cs
@@ -8,7 +8,10 @@ public class limitation : MonoBehaviour {
     private int buildIndex;
     public GameManager gm;
     public NewAudioManager audioMan;
+    
     public GameObject particleOnDestroy;
+    [SerializeField]
+    public ParticleSystem partSystem;
     // Use this for initialization
     void Start () {
         gm.updateBounces(limit - i);
@@ -27,9 +30,11 @@ public class limitation : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         i = i + 1;
+        Instantiate(partSystem.gameObject, transform.position, Quaternion.identity);
         gm.updateBounces(limit - i);
-
-        Debug.Log(i);
+     //   partSystem.transform.position = transform.position;
+   //     partSystem.Play();
+        Debug.Log("t " + i);
         if (i > limit)
         {
             //    SceneManager.LoadScene(buildIndex);

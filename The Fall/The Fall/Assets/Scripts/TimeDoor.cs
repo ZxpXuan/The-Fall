36ab61ddce4a31856201760e2e060d7b992332fc
Xiaoming Xu <DeanXUX@outlook.com>
2018-10-18 13:25:26 +0100

    no message

---
+++ The Fall/The Fall/Assets/Scripts/TimeDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDoor : MonoBehaviour {
+    public GameObject timedoo;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.collider.tag == "ball")
+        {
+            timedoo.GetComponent<Renderer>().enabled = false;
+            Invoke("ShowAgain", 3f);
+        }
+
+	}
+
+    public void ShowAgain()
+
+    {
+        timedoo.GetComponent<Renderer>().enabled = true;
+    }
+
+
+}
---
+++ The Fall/The Fall/Assets/Scripts/Useingtimes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Useingtimes : MonoBehaviour {
+    private int times;
+    public int maxtime = 3;
+    public GameObject dis;
+	// Use this for initialization
+	void Start () {
+        times = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerExit(Collider other)
+	{
+        if (other.tag == "ball") 
+        {
+            times = times + 1;
+        }
+        if (times > maxtime)
+        {
+            Destroy(dis);
+        }
+	}
+
+}
+
